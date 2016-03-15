80bb4a8e9818c859809a0d8615cfad362a099a06
jaga <jaga@MacBook-Air-de-jaga.local>
2016-03-15 17:24:40 -0600

    one more

--- pagefour/settings.py
+++ pagefour/settings.py
@@ -60,17 +60,17 @@ WSGI_APPLICATION = 'pagefour.wsgi.application'
 import dj_database_url
 DATABASES = {}
 DATABASES['default']=dj_database_url.config()
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'pagefour',
-#        'USER': 'julian',
-#        'PASSWORD': 'JuliaN',
-#        'HOST': '',
-#        'PORT': '',
-#    }
-#}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pagefour',
+#         'USER': 'julian',
+#         'PASSWORD': 'JuliaN',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+#
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
